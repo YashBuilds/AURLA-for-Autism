5a69cb4bf6cfaa46a2f34accec2713c1ebb9416c
UBER-VIDEO <yashvloggs20@gmail.com>
2025-10-07 10:47:45 +0530

    fix: TypeScript error in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import { ReactNode } from 'react'
 
 const poppins = Poppins({ 
   weight: ['400', '500', '600', '700', '800'],
@@ -19,7 +20,7 @@ export const metadata = {
   },
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
